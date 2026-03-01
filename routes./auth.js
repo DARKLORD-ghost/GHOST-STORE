b334c3748af795a98276c163020177ae29c78c39
DARKLORD-ghost <sfyallhmstfy067@gmail.com>
2026-03-01 12:49:50 +0200

    Create auth.js

---
+++ routes./auth.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashed
+  });
+
+  await user.save();
+  res.json({ message: "User created" });
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json({ message: "User not found" });
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.status(400).json({ message: "Wrong password" });
+
+  const token = jwt.sign({ id: user._id, role: user.role }, "SECRET_KEY");
+
+  res.json({ token });
+});
+
+module.exports = router;
